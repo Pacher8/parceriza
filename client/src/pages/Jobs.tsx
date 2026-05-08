84477d90a5de1adeb8bd5ec0c46de659069b4ce5
Gustavo Pacher <gustavo.pacher@preddita.com>
2026-05-07 22:23:26 -0300

    fix: add missing Link import in Jobs marketplace page
    White screen caused by Link from react-router-dom being used but not imported.

--- client/src/pages/Jobs.tsx
+++ client/src/pages/Jobs.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { NavBar } from '../components/NavBar';
 
 type AdFeed = { id: string; titulo: string; descricao: string; mediaUrl: string | null; posicionamento: string };
